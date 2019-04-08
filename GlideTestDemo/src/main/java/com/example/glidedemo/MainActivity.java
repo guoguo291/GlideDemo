f7ec2171d2e34ee5940db13212a55a09ab2131fb
guoj <guoj@landicorp.com>
2019-04-08 16:16:42 +0800

    添加依赖库 增加网络权限 完成主界面加载图片功能

--- GlideTestDemo/src/main/java/com/example/glidedemo/MainActivity.java
+++ GlideTestDemo/src/main/java/com/example/glidedemo/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.glidedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loadBtn;
+    private ImageView iv_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        iv_show = (ImageView) findViewById(R.id.iv_show);
+        loadBtn = (Button) findViewById(R.id.id_btn_load_img);
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+                Glide.with(MainActivity.this).load(url).into(iv_show);
+            }
+        });
     }
 }
